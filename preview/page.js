5a3582867267551e79d194bd32808d40403e4b17
ByteLight Plugin Repositories <plugins@bytelight.com>
2013-07-15 12:32:34 -0700

    Saved at: 2013-07-15 19:32:32 +0000

--- preview/page.js
+++ preview/page.js
@@ -4,7 +4,7 @@ if (BL.isTablet()) {
   var imageUrl = BL.getSettings().preview_image_url_tablet[0];
 }
 
-$('.digital-coupon').css('background', '#313131 url(' + imageUrl + ') no-repeat center 24px');
+$('.digital-coupon').css('background', '#313131 url(' + imageUrl + ') no-repeat center 0px');
 
 $('.digital-coupon').on('click', function() {
   BL.displayApp(APP.appId);
